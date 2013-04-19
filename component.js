c7998933e9eac21273d99992267ee40520d5ab57
Li Ge <lge@ucsd.edu>
2013-04-19 10:00:56 -0700

    Create component.js

---
+++ component.js
@@ -0,0 +1,16 @@
+
+{
+  "name": "bootstrap-file-input",
+  "repo": "lge88/bootstrap-file-input.git",
+  "upstream": "git://github.com/grevory/bootstrap-file-input.git",
+  "dependencies": {
+    "lge88/bootstrap": "*",
+    "lge88/jquery": "*"
+  },
+  "scripts": [
+    "bootstrap.file-input.js"
+  ],
+  "styles": [],
+  "templates": [],
+  "main": "bootstrap.file-input.js"
+}
